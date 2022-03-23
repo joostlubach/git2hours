8fa475453b916735709e050331de981699754d1e
Joost Lubach <joostlubach@gmail.com>
2022-03-23 15:04:44 +0100

    Error handling.

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@ extern crate derive_builder;
 mod git;
 mod counter;
 
-use std::error::Error;
+use std::{error::Error, io};
+use std::io::prelude::*;
 use counter::Counter;
 use git::CommitQuery;
 use itertools::Itertools;
@@ -21,13 +22,21 @@ fn main() -> Result<(), Box<dyn Error>> {
   let mut commits = query.run()?;
   let counter = Counter::new(&mut commits);
 
+  Ok(write_hours(counter).unwrap_or(()))
+}
+
+fn write_hours(counter: Counter) -> Result<(), io::Error> {
   let grouped_by_day = counter.group_by(|(commit, _)| {
     commit.date.date()
   });
 
+  let stdout = io::stdout();
+  let mut stdout = stdout.lock();
+
+
   for (date, commits) in &grouped_by_day {
     let hours: f32 = commits.map(|(_, hours)| hours).sum();
-    println!("{}: {:.0} hours", date.format("%Y-%m-%d [%a]"), hours);
+    writeln!(stdout, "{}: {:.0} hours", date.format("%Y-%m-%d [%a]"), hours)?;
   }
   
   Ok(())
